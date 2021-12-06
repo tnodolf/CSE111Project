a0ea6dbb37d3e2b04e8c28796e9ccf8f009aad4d
Sargune Kalsi <sargunekalsi@Sargunes-Air.attlocal.net>
2021-12-05 17:06:53 -0800

    built in league/team functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #flask imports
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DecimalField, SelectField
@@ -78,16 +78,34 @@ def sport():
         name = StringField('Sport Name:', validators=[DataRequired()])
         submit = SubmitField('Submit')
 
+    class LeagueForm(FlaskForm):
+        query = '''select s_name from sport order by s_name'''
+        conn = sqlite3.connect(database)
+        result = conn.execute(query)
+        sportsNames = []
+        for row in result:
+            sportsNames.append(row[0])
+        leagueName = StringField('League Name:', validators=[DataRequired()])
+        sport = SelectField('Sport Name:', choices=sportsNames, validators=[DataRequired()])
+        submitLeague = SubmitField('Submit')
+        
+
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
 
-    form = SportForm()
+    sportSubmit = SportForm()
+    leagueSubmit = LeagueForm()
     message = ""
+
+    sportNames = []
+    sportQuery = '''SELECT s_name from sport order by s_name'''
+    result = conn.execute(sportQuery)
+    for row in result:
+        sportNames.append(row[0])
     
 
-    if form.validate_on_submit():
-
-        pre_check = '''SELECT count(*) from sport where s_name = "{}" '''.format(form.name.data)
+    if sportSubmit.validate_on_submit():
+        pre_check = '''SELECT count(*) from sport where s_name = "{}" '''.format(sportSubmit.name.data)
         result = conn.execute(pre_check)
         val = 0
 
@@ -95,16 +113,56 @@ def sport():
             val = row[0]
 
         if (val != 0):
-            error_message = "The sport {} already exists.".format(form.name.data)
-            return render_template('sport.html', form=form, message=error_message, sportNames=sportNames)
+            error_message = "The sport {} already exists.".format(sportSubmit.name.data)
+            sportNames = []
+            sportQuery = '''SELECT s_name from sport'''
+            result = conn.execute(sportQuery)
+            for row in result:
+                sportNames.append(row[0])
+            return render_template('sport.html', sportForm=sportSubmit, leagueForm=leagueSubmit, message=error_message, sportNames=sportNames)
 
-        query = '''INSERT INTO SPORT VALUES ('{}', 0, 0)'''.format(form.name.data)
+        query = '''INSERT INTO SPORT VALUES ('{}', 0, 0)'''.format(sportSubmit.name.data)
         cursor.execute(query)
         conn.commit()
-        print("created sport: {} in db".format(form.name.data))
+        print("created sport: {} in db".format(sportSubmit.name.data))
+        class LeagueFormNew(FlaskForm):
+            query = '''select s_name from sport order by s_name'''
+            conn = sqlite3.connect(database)
+            result = conn.execute(query)
+            sportsNames = []
+            for row in result:
+                sportsNames.append(row[0])
+            leagueName = StringField('League Name:', validators=[DataRequired()])
+            sport = SelectField('Sport Name:', choices=sportsNames, validators=[DataRequired()])
+            submitLeague = SubmitField('Submit')
+        leagueSubmit = LeagueFormNew()
     
+
+    if leagueSubmit.validate_on_submit():
+        pre_check = '''SELECT count(*) from league where l_name = "{}" and l_sportsname = "{}" '''.format(leagueSubmit.leagueName.data, leagueSubmit.sport.data)
+        result = conn.execute(pre_check)
+        val = 0
+
+        for row in result:
+            val = row[0]
+
+        if val != 0:
+            error_message = "The league {} already exists within the sport {}.".format(leagueSubmit.leagueName.data, leagueSubmit.sport.data)
+            return render_template('sport.html', sportForm=sportSubmit, leagueForm=leagueSubmit, message=error_message, sportNames=sportNames)
+
+        query = '''INSERT INTO LEAGUE VALUES ("{}", "{}", 0, IFNULL( ((SELECT MAX(l_leaguekey) from league) + 1), 0))'''.format(leagueSubmit.sport.data, leagueSubmit.leagueName.data)
+        cursor.execute(query)
+        conn.commit()
+        print("created league: {} in db".format(leagueSubmit.leagueName.data))
+
+        updateSport = '''UPDATE Sport SET s_numleagues = s_numleagues + 1 WHERE s_name = "{}" '''.format(leagueSubmit.sport.data)
+        cursor.execute(updateSport)
+        conn.commit()
+        print("incremented sports")
+
+
     sportNames = []
-    sportQuery = '''SELECT s_name from sport'''
+    sportQuery = '''SELECT s_name from sport order by s_name'''
     result = conn.execute(sportQuery)
     for row in result:
         sportNames.append(row[0])
@@ -112,7 +170,7 @@ def sport():
 
     print(sportNames)
 
-    return render_template('sport.html', form=form, message=message, sportNames=sportNames)
+    return render_template('sport.html', sportForm=sportSubmit,leagueForm=leagueSubmit, message=message, sportNames=sportNames)
 
 @app.route('/referee', methods=['GET', 'POST'])
 def referee():
@@ -176,10 +234,80 @@ def referee():
     return render_template('referee.html', form=form, message=message, leagueNames=leagueNames)
 
 
+@app.route('/team', methods=['GET', 'POST'])
+def team():
+    class teamForm(FlaskForm):
+        query = '''select s_name from sport'''
+        conn = sqlite3.connect(database)
+        result = conn.execute(query)
+        sportsNames = []
+        for row in result:
+            sportsNames.append(row[0])
+        sportName = SelectField('Sport Name:', choices = sportsNames)
+        leagueName = SelectField('League Name:'  ,choices = [], validate_choice=False)
+        teamName = StringField('Team Name:')
+        submit = SubmitField('Submit')
+
+    team = teamForm()
+
+    message = ""
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+
+    if team.validate_on_submit():
+        findLeagueKey = '''select l_leaguekey from League where l_name="{}" and l_sportsname="{}" '''.format(team.leagueName.data, team.sportName.data)
+        leagueKey = None
+        result = conn.execute(findLeagueKey)
+        for row in result:
+            leagueKey = row[0]
+        
+        createTeam = '''INSERT INTO team values ("{}", 0, "{}")'''.format(leagueKey, team.teamName.data)
+        cursor.execute(createTeam)
+        conn.commit()
+        print("created team {}".format(team.teamName.data))
+
+        updateLeague = '''UPDATE League SET l_numteams = l_numteams + 1 WHERE l_leaguekey = "{}" '''.format(leagueKey)
+        cursor.execute(updateLeague)
+        conn.commit()
+        print("updated league")
+
+        updateSport = '''UPDATE Sport SET s_numteams = s_numteams + 1 WHERE s_name = "{}" '''.format(team.sportName.data)
+        cursor.execute(updateSport)
+        conn.commit()
+        print("updated sport")
+
+        createRecord = '''INSERT INTO Record VALUES ("{}", 0, 0)'''.format(team.teamName.data)
+        cursor.execute(createRecord)
+        conn.commit()
+        print("created record")
+
+
+    return render_template('team.html', form=team, message=message)
+
+
+@app.route('/get-league/<sport>')
+def get_league(sport):
+    sport = sport.replace('%20', ' ')
+    conn = sqlite3.connect(database)
+    query = '''select l_name from league where l_sportsname = "{}" '''.format(sport)
+    result = conn.execute(query)
+    leagueNames = []
+    for row in result:
+        leagueObj = {}
+        leagueObj['name'] = row[0]
+        leagueNames.append(leagueObj)
+    
+    return jsonify({'league_names' : leagueNames})
+    
+
+
+
+
+
 
 '''
 TOD-O:
- Pages for ingestion: league, match, record, team
+ Pages for ingestion: match, record
  ***sorting page****
 '''
 
